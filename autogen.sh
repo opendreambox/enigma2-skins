b3a46e566b5e6f6904d087d4a930494b094758cd
Andreas Oberritter <obi@opendreambox.org>
2017-04-20 17:34:54 +0200

    build: remove unused stuff

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-autoreconf --force --install
-
-echo "now, execute ./configure"
-echo "after that, you could build an ipkg with 'make ipkg' in any of the"
-echo "directories which support the ipkg target."
